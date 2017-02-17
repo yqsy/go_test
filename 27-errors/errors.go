0d8689ffc52c19ce98994868d0ffc023f8ab2663
yqsy <yqsy@126.com>
2017-02-17 23:55:49 +0800

    errors

---
+++ 27-errors/errors.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type MyError struct {
+	When time.Time
+	What string
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprintf("at %v, %s",
+		e.When, e.What)
+}
+
+func (e MyError) String() string {
+	return "xxx"
+}
+
+// Maybe error是MyError的接口
+func run() error {
+	return &MyError{
+		time.Now(),
+		"it didn't work",
+	}
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(MyError{time.Now(), "123"})
+}
